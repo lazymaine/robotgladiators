04e28f14e5a83977df5f5a8fba472e8c40857026
Cassidy Alex <cassidyalex@Cassidys-MBP.lan>
2022-10-18 11:24:54 -0400

    create and execute playerName and fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("this is an alert! JavaScript is running!")
+var playerName = window.prompt("What is your robot's name?")
+console.log("Our r+obot's name is "  playerName);
+
+function fight() {
+    window.alert("The fight has begun!")
+}
+
+// fight();
